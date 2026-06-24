81319aed42acd2f581f45d9e6eb7a48bb2ac5dd9
Sofia Orozco <sofiamen288@gmail.com>
2026-06-23 20:04:39 -0600

    Estructura de iteraciones

--- src/main/java/com/matapli3/proyectonewtonraphson/Logica/Iteracion.java
+++ src/main/java/com/matapli3/proyectonewtonraphson/Logica/Iteracion.java
@@ -1,5 +1,25 @@
 package com.matapli3.proyectonewtonraphson.Logica;
 
 public class Iteracion {
+    public int numero;
+    public double xi;
+    public double fx;
+    public double fpx;
+    public double xiNext;
+    public double delta;
 
+    public Iteracion(int numero, double xi, double fx, double fpx, double xiNext) {
+        this.numero = numero;
+        this.xi = xi;
+        this.fx = fx;
+        this.fpx = fpx;
+        this.xiNext = xiNext;
+        this.delta = Math.abs((xiNext - xi)/xiNext);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Iteración %d: xi = %.6f, f(x) = %.6f, f'(x) = %.6f, xi+1 = %.6f, δ = %.6f",
+                numero, xi, fx, fpx, xiNext, delta);
+    }
 }
